8c839c71c9c2c60cb57b34d9de53aede83722884
Sam Thomson <samt@samt.st>
2018-02-20 17:59:07 +0700

    tiny refactor to make more readable and debugging easier

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@ import axios from 'axios'
 
 export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number | null> {
     try{
-        let apiResponse = await axios.get(
-            `https://api.coinmarketcap.com/v1/ticker/${sCurrencyCode}/`
-        ).then(data => Number(data.data[0].price_usd))
+        let sAPIURL = `https://api.coinmarketcap.com/v1/ticker/${sCurrencyCode}/`
+        let apiResponse = await axios.get(sAPIURL).then(data => Number(data.data[0].price_usd))
         return apiResponse
     }catch(err){ return null }
 }
