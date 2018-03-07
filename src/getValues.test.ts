e1ea19f7f0700469f0643e564dffc3c10f682a8c
Sam Thomson <samt@samt.st>
2018-03-07 21:38:35 +0700

    tests... :)

--- src/getValues.test.ts
+++ src/getValues.test.ts
@@ -1,23 +1,88 @@
 import { expect } from 'chai'
-
 import * as sinon from 'sinon'
-
-
-
+import axios from 'axios'
+import nock = require('nock')
 import { getCryptoUSDValue } from './getValues'
 
 describe('getCryptoUSDValue function test', async () => {
     beforeEach(function () {
-      this.sandbox = sinon.sandbox.create()
     })
     
     afterEach(function () {
-      this.sandbox.restore()
+      nock.cleanAll()
     })
 
-    it('should return value', async () => {
-        var result = await getCryptoUSDValue('ethereum')
-        expect(result).to.exist
-        expect(result).to.be.a('number')
-    });
-});
+    it('should return value as expected', async () => {
+      
+      // stub response
+      // const apiStub = this.sandbox.stub(axios,' get', function (url))
+      nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/ethereum/')
+        .reply(200, `
+          [
+            {
+                "id": "ethereum", 
+                "name": "Ethereum", 
+                "symbol": "ETH", 
+                "rank": "2", 
+                "price_usd": "10000000", 
+                "price_btc": "0.0743", 
+                "24h_volume_usd": "1840370000.0", 
+                "market_cap_usd": "77113918221.0", 
+                "available_supply": "98040084.0", 
+                "total_supply": "98040084.0", 
+                "max_supply": null, 
+                "percent_change_1h": "-0.93", 
+                "percent_change_24h": "-5.28", 
+                "percent_change_7d": "-9.55", 
+                "last_updated": "1520432652"
+            }
+          ]
+        `)
+
+
+      var result = await getCryptoUSDValue('ethereum')
+      expect(result).to.exist
+      expect(result).to.be.a('number')
+      expect(result).to.equal(10000000)
+    })
+
+    it('should return null on bad response', async () => {
+      nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/ethereum/')
+        .reply(500, '')
+
+
+      var result = await getCryptoUSDValue('ethereum')
+      expect(result).to.equal(null)
+    })
+
+    it('should return null on bad data', async () => {
+      nock('https://api.coinmarketcap.com')
+        .get('/v1/ticker/ethereum/')
+        .reply(200, `
+          [
+            {
+                "id": "ethereum", 
+                "name": "Ethereum", 
+                "symbol": "ETH", 
+                "rank": "2", 
+                "price_btc": "0.0743", 
+                "24h_volume_usd": "1840370000.0", 
+                "market_cap_usd": "77113918221.0", 
+                "available_supply": "98040084.0", 
+                "total_supply": "98040084.0", 
+                "max_supply": null, 
+                "percent_change_1h": "-0.93", 
+                "percent_change_24h": "-5.28", 
+                "percent_change_7d": "-9.55", 
+                "last_updated": "1520432652"
+            }
+          ]
+        `)
+
+
+      var result = await getCryptoUSDValue('ethereum')
+      expect(result).to.equal(null)
+    })
+})
--- src/getValues.ts
+++ src/getValues.ts
@@ -3,7 +3,13 @@ import axios from 'axios'
 export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number | null> {
     try{
         let sAPIURL = `https://api.coinmarketcap.com/v1/ticker/${sCurrencyCode}/`
-        let apiResponse = await axios.get(sAPIURL).then(data => Number(data.data[0].price_usd))
+        let apiResponse = await axios.get(sAPIURL).then(data => {
+            if (data.data && data.data.length > 0 && data.data[0].price_usd) {
+                // has returned the data we expected
+                return Number(data.data[0].price_usd)
+            }
+            return null
+        })
         return apiResponse
     }catch(err){ return null }
 }
