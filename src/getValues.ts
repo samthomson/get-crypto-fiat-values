b86edc5eeeb9735bfaef93aed29f6c93109d0d07
Sam Thomson <samt@samt.st>
2018-03-08 23:00:26 +0700

    get multiple values

--- src/getValues.ts
+++ src/getValues.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { CurrencyValues } from './types'
 
 export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number | null> {
     try{
@@ -7,6 +8,33 @@ export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number
             if (data.data && data.data.length > 0 && data.data[0].price_usd) {
                 // has returned the data we expected
                 return Number(data.data[0].price_usd)
+            }
+            return null
+        })
+        return apiResponse
+    }catch(err){ return null }
+}
+export async function getMultipleCryptoUSDValue (saCurrencyCodes: string[]) : Promise<CurrencyValues | null> {
+    try{
+        let sAPIURL = `https://api.coinmarketcap.com/v1/ticker/?limit=0`
+        let apiResponse = await axios.get(sAPIURL).then(data => {
+            // parse out currencies we're interested in
+
+            if (data.data && data.data.length > 0 && data.data[0].price_usd) {
+                let currencies = data.data
+                let oReturnCurrencies: CurrencyValues = {}
+
+                for (let iIndex = 0; iIndex < currencies.length; iIndex++) {
+                    if (saCurrencyCodes.indexOf(currencies[iIndex].id) > -1) {
+                        // one of the currencies we're interested in
+                        const { id, price_usd } = currencies[iIndex]
+                        oReturnCurrencies[id] = {
+                            usdValue: Number(price_usd)
+                        }
+                    }
+                }
+                // has returned the data we expected
+                return oReturnCurrencies
             }
             return null
         })
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface CurrencyValues {
+    [currency: string]: {
+        usdValue: number
+    }
+}
