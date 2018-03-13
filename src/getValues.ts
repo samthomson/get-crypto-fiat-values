28f0b9468ca1008157e42156c4f55202768e3557
Sam Thomson <samt@samt.st>
2018-03-13 08:25:04 +0700

    minor refactor

--- src/getValues.ts
+++ src/getValues.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { CurrencyValues, CMCCurrencySnapshot } from './types'
 
 export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number | null> {
@@ -17,28 +17,26 @@ export async function getCryptoUSDValue (sCurrencyCode: string) : Promise<number
 export async function getMultipleCryptoUSDValue (saCurrencyCodes: string[]) : Promise<CurrencyValues | null> {
     try{
         let sAPIURL = `https://api.coinmarketcap.com/v1/ticker/?limit=0`
-        let apiResponse = await axios.get(sAPIURL).then(data => {
-            // parse out currencies we're interested in
+        const data: AxiosResponse = await axios.get(sAPIURL)
+        // parse out currencies we're interested in
 
-            if (data.data && data.data.length > 0 && data.data[0].price_usd) {
-                let currencies = data.data
-                let oReturnCurrencies: CurrencyValues = {}
+        if (data.data && data.data.length > 0 && data.data[0].price_usd) {
+            let currencies = data.data
+            let oReturnCurrencies: CurrencyValues = {}
 
-                for (let iIndex = 0; iIndex < currencies.length; iIndex++) {
-                    if (saCurrencyCodes.indexOf(currencies[iIndex].id) > -1) {
-                        // one of the currencies we're interested in
-                        const { id, price_usd } = currencies[iIndex]
-                        oReturnCurrencies[id] = {
-                            usdValue: Number(price_usd)
-                        }
+            for (let iIndex = 0; iIndex < currencies.length; iIndex++) {
+                if (saCurrencyCodes.indexOf(currencies[iIndex].id) > -1) {
+                    // one of the currencies we're interested in
+                    const { id, price_usd } = currencies[iIndex]
+                    oReturnCurrencies[id] = {
+                        usdValue: Number(price_usd)
                     }
                 }
-                // has returned the data we expected
-                return oReturnCurrencies
             }
-            return null
-        })
-        return apiResponse
+            // has returned the data we expected
+            return oReturnCurrencies
+        }
+        return null
     }catch(err){ return null }
 }
 export async function getAllCryptoValues (): Promise<CMCCurrencySnapshot[] | null> {
